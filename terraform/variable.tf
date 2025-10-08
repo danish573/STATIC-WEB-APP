f33610d1972a46dc906f878291d8bb1015a88628
danish573 <dkhan573@gmail.com>
2025-10-08 17:29:45 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,29 +1,29 @@
 variable "aws_region" {
   description = "AWS Region to setup Server"
-  type = string
-  default = "ua-east-1"
+  type        = string
+  default     = "ap-south-1"
 }
 
 variable "s3_bucket_name" {
   description = "S3 BucketUnique Name"
-  type = string
-  default = "static_website_s3_bucket"
+  type        = string
+  default     = "static-website-s3-bucket-573"
 }
 
 variable "ami_id" {
   description = "Ubuntu Image ID"
-  type = string
-  default = "ami-0c7217cdde317cfec" # Ubuntu 22.04 LTS (us-east-1)"
+  type        = string
+  default     = "ami-02d26659fd82cf299" # Ubuntu 22.04 LTS (us-east-1)
 }
 
 variable "instance_type" {
   description = "Define EC-2 instance type"
-  type = string
-  default = "t2.micro"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "key_name" {
   description = "EC-2 Instance Security key"
-  type = string
-  default = "key"
+  type        = string
+  default     = "Mumbai"
 }
