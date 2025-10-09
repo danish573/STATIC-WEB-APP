6f8b3ff1e033d7428c09874b5f071bb9725fef04
danish573 <dkhan573@gmail.com>
2025-10-09 17:42:52 +0530

    Update minify_and_deploy.sh

--- scripts/minify_and_deploy.sh
+++ scripts/minify_and_deploy.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 set -e
 
-COMMAND= $1
-SURCE_DIR= "src"
-BUILD_DIR= "build"
+COMMAND=$1
+SOURCE_DIR="src"
+BUILD_DIR="build"
 
-if["$COMMAND" = "minify"]; then
-    echo "Cleaning old build directory"
-    rm -rf $BUILD_DIR
-    mkdir -p $BUILD_DIR
+if [ "$COMMAND" = "minify" ]; then
+    echo "🧹 Cleaning old build directory..."
+    rm -rf "$BUILD_DIR"
+    mkdir -p "$BUILD_DIR"
 
-    echo "Minifying HTML, CSS and JS...."
-    cp -r $SOURCE_DIR/* $BUILD_DIR/
-    find $BUILD_DIR -type f -name "*.html" -exec minify {} -o {} \;
-    find $BUILD_DIR -type f -name "*.css" -exec minify {} -o {} \;
-    find $BUILD_DIR -type f -name "*.js" -exec minify {} -o {} \;
+    echo "✨ Minifying HTML, CSS, and JS..."
+    cp -r "$SOURCE_DIR"/* "$BUILD_DIR"/
+
+    find "$BUILD_DIR" -type f -name "*.html" -exec minify {} -o {} \;
+    find "$BUILD_DIR" -type f -name "*.css" -exec minify {} -o {} \;
+    find "$BUILD_DIR" -type f -name "*.js" -exec minify {} -o {} \;
+
+    echo "✅ Minification complete!"
+fi
